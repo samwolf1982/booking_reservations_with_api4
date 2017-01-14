821e91b41f53789ae2bcedfdfe0c152233a7ec78
samwolf1982 <biryukwolf@yahoo.com>
2017-01-14 03:04:15 +0200

    claer file

---
+++ locallibs/sabre/Sabre.php
@@ -0,0 +1,15 @@
+<?php
+namespace SabreLibs;
+
+class Hotels{
+
+       public function  __construct($name)
+      {
+         $this->name=$name;
+      }
+      public function test($value='')
+      {
+       echo "string";
+      }
+
+  }

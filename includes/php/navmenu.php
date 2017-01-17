677fe295c6973c4273abc54dab317f720e3c9129
samwolf1982 <biryukwolf@yahoo.com>
2017-01-17 15:11:22 +0200

    translate naw menu

--- includes/php/navmenu.php
+++ includes/php/navmenu.php
@@ -14,11 +14,11 @@
         <div class="menu_block">
           <nav class="horizontal-nav full-width horizontalNav-notprocessed">
             <ul class="sf-menu">
-             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == '') {;?>class="current"<?}?>><a href="http://www.bookingroll.com/"><span class="fa fa-home"></span>Ana Sayfa</a></li>
+             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == '') {;?>class="current"<?}?>><a href="http://www.bookingroll.com/"><span class="fa fa-home"></span>Home page</a></li>
 
 <!--              naw menu UNCOMENT !!!!!!!!!!!!!! -->
-          <!--    <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Turlar') {;?>class="current"<?}?>><a href="Turlar.php"><span class="fa fa-globe"></span>Turlar<strong></strong></a>
-				<ul>
+             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Turlar') {;?>class="current"<?}?>><a href="Turlar.php"><span class="fa fa-globe"></span>Tours<strong></strong></a>
+			<!-- 	<ul>
                   <li><a href="Turlar.php?t=yurtdisi">Yurtdışı Turlar</a></li>
                   <li><a href="Turlar.php?t=yurtici">Yurtiçi Turlar</a></li>
                   <li><a href="Turlar.php?t=kultur">Kültür Turları</a></li>
@@ -31,11 +31,11 @@
                   </li>
                 </ul>
 			 </li> -->
-             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Otel') {;?>class="current"<?}?>><a href="/Hotels.php"><span class="fa fa-bed"></span>Otel</a></li>
-             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Ucak') {;?>class="current"<?}?>><a href="Ucak.php"><span class="fa fa-plane"></span>Uçak</a></li>
-             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Gemi') {;?>class="current"<?}?>><a href="Gemi.php"><span class="fa fa-ship"></span>Gemi</a></li>
-             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Tatil') {;?>class="current"<?}?>><a href="Tatil.php"><span class="fa fa-car"></span>Araç</a></li>
-             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Sigorta') {;?>class="current"<?}?>><a href="Sigorta.php"><span class="fa fa-umbrella"></span>Sigorta</a></li>
+             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Otel') {;?>class="current"<?}?>><a href="/Hotels.php"><span class="fa fa-bed"></span>Hotel</a></li>
+             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Ucak') {;?>class="current"<?}?>><a href="Ucak.php"><span class="fa fa-plane"></span>Airplane</a></li>
+             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Gemi') {;?>class="current"<?}?>><a href="Gemi.php"><span class="fa fa-ship"></span>Ship</a></li>
+             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Tatil') {;?>class="current"<?}?>><a href="Tatil.php"><span class="fa fa-car"></span>Car</a></li>
+             <li <?if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'Sigorta') {;?>class="current"<?}?>><a href="Sigorta.php"><span class="fa fa-umbrella"></span>Insurance</a></li>
            </ul>
           </nav>
           <div class="clear"></div>       
